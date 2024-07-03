b5e63528457123a5425c8a493aa1609482186c73
marampalli <90436037+marampalli@users.noreply.github.com>
2024-07-03 18:10:03 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "web" {
+/*resource "aws_instance" "web" {
   ami           = "ami-022ce6f32988af5fa"
   instance_type = "t2.micro"
 
@@ -6,3 +6,4 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
   }
 }
+*/
