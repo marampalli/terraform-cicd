9c8522f19b9fb72a1145877a1020426569fbcd72
marampalli <90436037+marampalli@users.noreply.github.com>
2024-07-03 16:33:04 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,18 +1,16 @@
 terraform {
-  required_version = ">= 1.6" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = ">= 5.0"
+      version = "3.1.0"
     }
-  } 
+  }
   backend "s3" {
     bucket = "srini-terraform-statefile"
     key    = "dev/dev.tfstate"
     region = "ap-south-1"
   }
 }  
- 
 # Provider Block
   provider "aws" {
   region = "ap-south-1"
