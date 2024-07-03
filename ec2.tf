bc8e3a2a15cf9ea23c621c8857b1e11f7abd393a
marampalli <90436037+marampalli@users.noreply.github.com>
2024-07-03 19:39:04 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -7,3 +7,13 @@
   }
 }
 */
+resource "aws_instance" "jenkins"{
+  ami = "ami-0cc9838aa7ab1dce7"
+  instance_type = "t2.micro"
+  key_name = "terraform-key"
+  tags = {
+    Name = "Jenkins Server"
+    Terraform   = "true"
+    Environment = "dev"
+  } 
+}
