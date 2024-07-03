972896fd2049f3779c9d3f1bea484ea141aa0c5c
marampalli <90436037+marampalli@users.noreply.github.com>
2024-07-03 16:20:16 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,25 +1,6 @@
-# Create a new instance of the latest Ubuntu 20.04 on an
-# t3.micro node with an AWS Tag naming it "HelloWorld"
-
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["339713130215"] # Canonical
-}
-
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  ami           = ami-022ce6f32988af5fa
+  instance_type = "t2.micro"
 
   tags = {
     Name = "HelloWorld"
