3729a9b772e03a7c74bafb1d8e5cd64848ddfede
marampalli <90436037+marampalli@users.noreply.github.com>
2024-07-03 16:26:53 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -6,12 +6,13 @@ terraform {
       version = ">= 5.0"
     }
   } 
-}  
   backend "s3" {
     bucket = "srini-terraform-statefile"
     key    = "dev/dev.tfstate"
     region = "ap-south-1"
   }
+}  
+ 
 # Provider Block
   provider "aws" {
   region = "ap-south-1"
