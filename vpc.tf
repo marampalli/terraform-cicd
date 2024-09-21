c2e1789fed4248184a6ee00a32a9b6ccd5a5a99c
Srinivas <marampalli@gmail.com>
2024-09-21 11:13:13 +0400

    added files

--- vpc.tf
+++ vpc.tf
@@ -2,10 +2,10 @@ module "vpc" {
   source  = "git::https://github.com/terraform-aws-modules/terraform-aws-vpc.git"
   #version = "5.13.0"
 
-  name = "New-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  # VPC Basic Details
+  name = "${local.name}-${var.vpc_name}"
+  cidr = var.vpc_cidr_block
+  azs             = var.vpc_availability_zones
+  public_subnets  = var.vpc_public_subnets
+  private_subnets = var.vpc_private_subnets  
 }
