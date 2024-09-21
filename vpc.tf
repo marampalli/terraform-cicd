61e132a3da5c0083aa9a13229a490fb29c7433ca
Srinivas <marampalli@gmail.com>
2024-09-21 11:46:28 +0400

    updated vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -7,5 +7,13 @@ module "vpc" {
   cidr = var.vpc_cidr_block
   azs             = var.vpc_availability_zones
   public_subnets  = var.vpc_public_subnets
-  private_subnets = var.vpc_private_subnets  
+  private_subnets = var.vpc_private_subnets 
+
+  # Database Subnets
+  database_subnets = var.vpc_database_subnets
+  create_database_subnet_group = var.vpc_create_database_subnet_group
+  create_database_subnet_route_table = var.vpc_create_database_subnet_route_table
+  # create_database_internet_gateway_route = true
+  # create_database_nat_gateway_route = true
+   
 }
